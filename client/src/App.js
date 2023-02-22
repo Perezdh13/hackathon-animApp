e84b21d3cdfac71e648f97c7d49ffc714cf07a16
perezdh13@gmail.com <Perezdh13@gmail.com>
2023-02-22 10:52:39 +0100

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import '../src/css/App.css';
+
+function App() {
+
+  const router = createBrowserRouter([
+
+    {
+      path:"/",
+      element:<>   </>
+    },
+    {
+      path:"/",
+      element:<>   </>
+    },
+    {
+      path:"/",
+      element:<>   </>
+    },
+  ])
+
+  return (
+    <div className="App">
+      <h1>animApp </h1>
+
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
