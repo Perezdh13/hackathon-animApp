705906d5d305a868c0ef1b9f7450f9a8f8599672
perezdh13@gmail.com <Perezdh13@gmail.com>
2023-02-22 14:51:07 +0100

    add components

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
 import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import HeaderBar from './components/HeaderBar';
 import AddAnimals from './components/AddAnimals';
+import FooterBar from './components/FooterBar';
+import Search from './components/Search';
+import Carrousel from './components/Carrousel';
+import HomePage from './components/HomePage';
+import SingleAnimal from './components/SingleAnimal';
 import '../src/css/App.css';
 
 function App() {
