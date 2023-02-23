8ca8944dc27be898321e9dfea12084c47a62e298
Xavi1594 <xavi9415@hotmail.com>
2023-02-23 12:45:23 +0100

    mergeado

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,44 @@
-import {createBrowserRouter, RouterProvider} from 'react-router-dom';
-import AddAnimals from './components/AddAnimals';
-import FooterBar from './components/FooterBar';
-import Search from './components/Search';
-import Carrousel from './components/Carrousel';
-import HomePage from './components/HomePage';
-import HeaderBar from './components/headerBar'
-import SingleAnimal from './components/SingleAnimal';
-import Contact from './components/Contact';
-import '../src/css/App.css';
-import AboutMe  from './components/AboutMe';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AddAnimals from "./components/AddAnimals";
+import FooterBar from "./components/FooterBar";
+import Search from "./components/Search";
+import Carrousel from "./components/Carrousel";
+import HomePage from "./components/HomePage";
+import HeaderBar from "./components/HeaderBar";
+import SingleAnimal from "./components/SingleAnimal";
+import Contact from "./components/Contact";
+import "../src/css/App.css";
+import AboutMe from "./components/AboutMe";
+import { ReportarAbuso } from "./components/ReportarAbuso";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const router = createBrowserRouter([
-
     {
-      path:"/",
-      element:<><HeaderBar /> <AddAnimals/>  </>
+      path: "/",
+      element: (
+        <>
+          <HeaderBar /> <AddAnimals />{" "}
+        </>
+      ),
     },
     {
-      path:"/about",
-      element:<>    </>
+      path: "/about",
+      element: <> </>,
     },
     {
-      path:"/reportarabuso",
-      element:<> <ReportarAbuso />  </>
+      path: "/reportarabuso",
+      element: (
+        <>
+          {" "}
+          <ReportarAbuso />{" "}
+        </>
+      ),
     },
-  ])
+  ]);
 
   return (
     <div className="App">
-      
-      <Carrousel></Carrousel>
       <RouterProvider router={router} />
     </div>
   );
