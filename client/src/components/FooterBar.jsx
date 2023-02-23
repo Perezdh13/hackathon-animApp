ba56b613f85c29448eb3de53793e455112ef8873
VeronikaKoma <116543383+VeronikaKoma@users.noreply.github.com>
2023-02-23 12:23:43 +0100

    add logo

--- client/src/components/FooterBar.jsx
+++ client/src/components/FooterBar.jsx
@@ -6,9 +6,9 @@ function FooterBar() {
       <img src={require('../img/Dog Heart.png')} alt="" />
       <img src={require('../img/Pet Commands Train.png')} alt="" />
       <img src={require('../img/Turtle.png')} alt="" />
-      <img src={require('../img/instagram.png')} alt="" />
-      <img src={require('../img/facebook.png')} alt="" />
-      <img src={require('../img/twitter.png')} alt="" />
+      <img className ="logo" src={require('../img/instagram.png')} alt="" />
+      <img className ="logo" src={require('../img/facebook.png')} alt="" />
+      <img className ="logo" src={require('../img/twitter.png')} alt="" />
     </div>
   )
 }
