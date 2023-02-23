71cd92a3ff72334000d082ab9ac4fd7638bc8f94
irene <irenefl897@gmail.com>
2023-02-23 20:42:17 +0100

    change style report

--- client/src/components/Report.jsx
+++ client/src/components/Report.jsx
@@ -3,16 +3,16 @@ import React from "react";
 
 export const Report = () => {
   return (
-    <div>
-        <p>Si sabes de algún animal que esté sufriendo maltrato 
+    <div className="container">
+        <p className="report-text">Si sabes de algún animal que esté sufriendo maltrato 
 o abuso repórtalo a las autoridades competentes.
 </p>
 <hr />
-<h3>Mosos de escuadra  (112)</h3>
+<h3 className="report-text-h3">Mosos de escuadra  (112)</h3>
 
-<h3>Agents Rurals (93 574 00 36) (Cataluña)</h3>
+<h3 className="report-text-h3">Agents Rurals (93 574 00 36) (Cataluña)</h3>
 
-<h3>Guardia Civil - Servicio de Protección de la Naturaleza (SEPRONA):</h3> 
+<h3 className="report-text-h3">Guardia Civil - Servicio de Protección de la Naturaleza (SEPRONA):</h3> 
 </div>
 )
 }
