e941cdff4fe8bcefc01a78aca9f63d15ae03c7ae
VeronikaKoma <116543383+VeronikaKoma@users.noreply.github.com>
2023-02-23 12:12:26 +0100

    final footer

--- client/src/components/FooterBar.jsx
+++ client/src/components/FooterBar.jsx
@@ -4,6 +4,11 @@ function FooterBar() {
   return (
     <div className = "Container-fluid">
       <img src={require('../img/Dog Heart.png')} alt="" />
+      <img src={require('../img/Pet Commands Train.png')} alt="" />
+      <img src={require('../img/Turtle.png')} alt="" />
+      <img src={require('../img/instagram.png')} alt="" />
+      <img src={require('../img/facebook.png')} alt="" />
+      <img src={require('../img/twitter.png')} alt="" />
     </div>
   )
 }
