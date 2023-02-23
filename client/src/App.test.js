010019a2c9548d264d3ba3509ef2cafb227b0c11
Xavi1594 <xavi9415@hotmail.com>
2023-02-23 17:36:16 +0100

    volviendo a commit anterior

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import Contact from './components/Contact';
+import Carrousel from './components/Carrousel';
+import renderer from 'react-test-renderer';
 
 test('test contact', () => {
   render(<Contact />);
   const linkElement = screen.getByText(/Nombre/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Carousel component correctly', () => {
+  const component = render.create(
+    <Carrousel />
+  );
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
