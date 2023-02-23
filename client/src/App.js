4fd377deed9684f403527626f7a17333205670ba
VeronikaKoma <116543383+VeronikaKoma@users.noreply.github.com>
2023-02-23 13:33:24 +0100

    footer error2

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import HeaderBar from './components/HeaderBar';
 import AddAnimals from './components/AddAnimals';
 import FooterBar from './components/FooterBar';
+import './css/footer.css';
 import Search from './components/Search';
 import Carrousel from './components/Carrousel';
 import HomePage from './components/HomePage';
@@ -9,42 +10,26 @@ import SingleAnimal from './components/SingleAnimal';
 import '../src/css/App.css';
 import AboutMe  from './components/AboutMe';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './components/FooterBar';
-import './css/footer.css';
 
 function App() {
   const router = createBrowserRouter([
     {
       path:"/",
-      element:<><AddAnimals/> <FooterBar/></>
-      element:<><HeaderBar />   </>
+        element:<><HeaderBar/><Carrousel/><Search/><FooterBar/></>
     },
     {
       path:"/about",
       element:<> <HeaderBar /> <AboutMe />  </>
     },
-    {
-      path:"/",
-      element:<>   </>  
-      path: "/reportarabuso",
-      element: (
-        <>
-          {" "}
-          <ReportarAbuso />{" "}
-        </>
-      ),
-    },
+  
   ]);
 
   return (
     <div className="App">
-    
-      
-      <RouterProvider router={router} />
+          <RouterProvider router={router} />
     </div>
   );
 }
 
-export default App; 
-
+export default App;
 
